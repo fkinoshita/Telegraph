0d214dca3fd819bf3e1770e7aed937d2d0546437
Felipe Kinoshita <kinofhek@gmail.com>
2023-03-31 16:21:56 -0300

    application: Add gi "require version" lines
    This was still in the main.py file, let's move it to the application.py
    file and it should fix the warning.

--- src/application.py
+++ src/application.py
@@ -1,4 +1,8 @@
+import gi
 from gettext import gettext as _
+
+gi.require_version('Gtk', '4.0')
+gi.require_version('Adw', '1')
 
 from gi.repository import Adw, Gtk, Gio
 
--- src/main.py
+++ src/main.py
@@ -18,10 +18,6 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import sys
-import gi
-
-gi.require_version('Gtk', '4.0')
-gi.require_version('Adw', '1')
 
 from .application import TelegraphApplication
 
