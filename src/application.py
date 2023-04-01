3f70c60d91d52507acfbd5b2fcb7a3a6b6fa06a3
Felipe Kinoshita <kinofhek@gmail.com>
2023-03-31 21:18:24 -0300

    application: Organize code a bit
    This commit moves the actions and accelerators setup to thier own
    separate functions.

--- src/application.py
+++ src/application.py
@@ -11,15 +11,13 @@ from .window import TelegraphWindow
 
 class TelegraphApplication(Adw.Application):
     def __init__(self):
-        super().__init__(application_id='io.github.fkinoshita.Telegraph',
-                         flags=Gio.ApplicationFlags.FLAGS_NONE)
+        super().__init__(
+            application_id='io.github.fkinoshita.Telegraph',
+            flags=Gio.ApplicationFlags.FLAGS_NONE
+        )
 
-        quit_action = Gio.SimpleAction.new("quit", None)
-        quit_action.connect("activate", self.on_quit_action)
-        self.add_action(quit_action)
-        self.set_accels_for_action("app.quit", ["<Control>q", "<Control>w"])
-
-        self.create_action('about', self.on_about_action)
+        self.setup_actions()
+        self.add_global_accelerators()
 
 
     def do_activate(self):
@@ -34,6 +32,25 @@ class TelegraphApplication(Adw.Application):
         win.present()
 
 
+    def setup_actions(self):
+        quit_action = Gio.SimpleAction.new("quit", None)
+        quit_action.connect("activate", self.on_quit_action)
+        self.add_action(quit_action)
+
+        about_action = Gio.SimpleAction.new("about", None)
+        about_action.connect("activate", self.on_about_action)
+        self.add_action(about_action)
+
+
+    def add_global_accelerators(self):
+        self.set_accels_for_action("app.quit", ["<Control>q"])
+        self.set_accels_for_action("window.close", ["<Control>w"])
+
+
+    def on_quit_action(self, widget, _):
+        self.quit()
+
+
     def on_about_action(self, widget, _):
         """Callback for the app.about action."""
         builder = Gtk.Builder.new_from_resource(
@@ -42,24 +59,4 @@ class TelegraphApplication(Adw.Application):
         about_dialog = builder.get_object("about_dialog")
         about_dialog.set_transient_for(self.props.active_window)
         about_dialog.present()
-
-
-    def on_quit_action(self, widget, _):
-        self.quit()
-
-
-    def create_action(self, name, callback, shortcuts=None):
-        """Add an application action.
-
-        Args:
-            name: the name of the action
-            callback: the function to be called when the action is
-              activated
-            shortcuts: an optional list of accelerators
-        """
-        action = Gio.SimpleAction.new(name, None)
-        action.connect("activate", callback)
-        self.add_action(action)
-        if shortcuts:
-            self.set_accels_for_action(f"app.{name}", shortcuts)
 
